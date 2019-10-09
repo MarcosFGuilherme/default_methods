fc60261247216514ea1452e0a58641ca6bb2b75f
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-09 11:40:14 -0300

    Start project - Make input screen.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,21 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-
+		System.out.print("Amount: ");
+		double amount = sc.nextDouble();
+		System.out.print("Months: ");
+		int months = sc.nextInt();
+		
+		System.out.println("Payment after 3 months:");
+		System.out.println("[" + String.format("%.2f", amount) + ", " + months + "]");
+		
+		sc.close();
 	}
 
 }
